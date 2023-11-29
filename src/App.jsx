65a3de61d16501c74ec93e2889c51aa7607d3f70
FahimaGold <df_mokhtari@esi.dz>
2023-11-29 21:01:04 +0100

    add counter flag

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState, useRef, useLayoutEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [flag, toggleFlag] = useState(0);
+  
+  const handleClick = () => {
+    setCount( (prevCount) => prevCount + 1);
+    toggleFlag ((prevFlag) => prevFlag === 0? 1: 0);
+  }
+ 
+  return (
+    <>
+      <button onClick={handleClick}>counter-flag</button>
+      <p>Counter: {count}</p>
+      <p>flag: {flag}</p>
+    </>
+  )
+}
+
+export default App
