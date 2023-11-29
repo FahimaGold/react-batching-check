efbc110ced8638b1af1985af271a0f2d3b4c4815
FahimaGold <df_mokhtari@esi.dz>
2023-11-29 21:07:34 +0100

    add renders counter to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import useRendersCount from './hooks/useRendersCount';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
   const [flag, toggleFlag] = useState(0);
-  
+  const rendersCounter = useRendersCount();
   const handleClick = () => {
     setCount( (prevCount) => prevCount + 1);
     toggleFlag ((prevFlag) => prevFlag === 0? 1: 0);
@@ -15,6 +16,7 @@ function App() {
       <button onClick={handleClick}>counter-flag</button>
       <p>Counter: {count}</p>
       <p>flag: {flag}</p>
+      <p>Renders times: {rendersCounter}</p>
     </>
   )
 }
