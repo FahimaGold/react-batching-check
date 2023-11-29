362e73841e98f584c02b8de7075b7d9d7e085104
FahimaGold <df_mokhtari@esi.dz>
2023-11-29 21:23:07 +0100

    add timeout

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,20 @@ function App() {
     toggleFlag ((prevFlag) => prevFlag === 0? 1: 0);
   }
  
+  const handleClickTimeout = () => {
+    setTimeout(() => {
+    setCount( (prevCount) => prevCount + 1);
+    toggleFlag ((prevFlag) => prevFlag === 0? 1: 0);
+    }, 1000)
+  }
   return (
     <>
       <button onClick={handleClick}>counter-flag</button>
       <p>Counter: {count}</p>
       <p>flag: {flag}</p>
       <p>Renders times: {rendersCounter}</p>
-    </>
+      <button onClick={handleClickTimeout}>Timeout</button>
+     </>
   )
 }
 
